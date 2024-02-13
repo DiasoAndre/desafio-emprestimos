01828e53d8a52c50f5a236ce5713fcd6cb71221c
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-02-13 18:57:52 -0300

    feat: add loansController;

--- src/usecases/loans/loans-controller.js
+++ src/usecases/loans/loans-controller.js
@@ -1,7 +1,26 @@
-const { loansUseCase } = require('./loans-usecase')
+const { loansUseCase } = require("./loans-usecase");
+const { customerSchema } = require("../../schemas/CustomerSchema");
+
 
 const loansController = async (request, response) => {
+  const validation = customerSchema.safeParse(request.body);
 
-}
+  if (validation.success) {
+    const customer = validation.data;
 
-module.exports = { loansController }
+    const result = {
+      customer: customer.name,
+      loans: loansUseCase(customer),
+    };
+
+    return response.status(200).json(result);
+  } else {
+    return response
+      .status(400)
+      .json("The body of the request is outside of what is required");
+  }
+
+  return response.status(204);
+};
+
+module.exports = { loansController };
