886c9001951fca9981b430386b2f179978208d2a
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-02-13 18:43:52 -0300

    feat: add loans useCase;

---
+++ src/usecases/loans/loans-usecase.js
@@ -0,0 +1,22 @@
+const { data } = require("../../loans");
+
+const loansUseCase = async (customer) => {
+  let result = [];
+  const loans = data(customer);
+
+  loans.forEach((loan) => {
+    const include = loans.find((l) => l.name == loan.name);
+    if (!include) {
+      if (loan.requirements) {
+        result.push({
+            name: loan.name,
+            interest_rate: loan.interest_rate
+        })
+      }
+    }
+  });
+
+  return result
+};
+
+module.exports = { loansUseCase };
