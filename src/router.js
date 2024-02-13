4223fbc54d78803b150e8a27d4eb873210f74a9a
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-02-13 19:30:20 -0300

    feat: adds more routes including "/customer-loans";

--- src/router.js
+++ src/router.js
@@ -1,5 +1,14 @@
 const { Router } = require('express')
 
+const { loansController } = require('./usecases/loans/loans-controller')
+
 const router = Router()
+
+router.get('/', (__, response) => {
+
+    response.status(200).send('Hello World')
+})
+
+router.post('/customer-loans', loansController)
 
 module.exports = { router }
